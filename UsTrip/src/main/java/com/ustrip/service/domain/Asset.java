21b6b568ad13bf3124e4769284c995a65dea4ee0
user <user@Marina-PC>
2017-05-30 18:29:51 +0900

    RN :: 170530, updateBlog 진행

--- UsTrip/src/main/java/com/ustrip/service/domain/Asset.java
+++ UsTrip/src/main/java/com/ustrip/service/domain/Asset.java
@@ -1,5 +1,116 @@
 package com.ustrip.service.domain;
 
+import java.util.Date;
+
 public class Asset {
+	
+	private int assetNo;
+	private int travNo;
+	private int blogNo;
+	private Date visitDate;
+	private String usage;
+	private String assetCategory;
+	private int charge;
+	private String travTitle;
+	private Date startDate;
+	private Date endDate;
+	private int totalDate;
+	
+	public Asset(){		
+	}
+
+	public int getAssetNo() {
+		return assetNo;
+	}
+
+	public void setAssetNo(int assetNo) {
+		this.assetNo = assetNo;
+	}
+
+	public int getTravNo() {
+		return travNo;
+	}
+
+	public void setTravNo(int travNo) {
+		this.travNo = travNo;
+	}
+
+	public int getBlogNo() {
+		return blogNo;
+	}
+
+	public void setBlogNo(int blogNo) {
+		this.blogNo = blogNo;
+	}
+
+	public Date getVisitDate() {
+		return visitDate;
+	}
+
+	public void setVisitDate(Date visitDate) {
+		this.visitDate = visitDate;
+	}
+
+	public String getUsage() {
+		return usage;
+	}
+
+	public void setUsage(String usage) {
+		this.usage = usage;
+	}
+
+	public String getAssetCategory() {
+		return assetCategory;
+	}
+
+	public void setAssetCategory(String assetCategory) {
+		this.assetCategory = assetCategory;
+	}
+
+	public int getCharge() {
+		return charge;
+	}
+
+	public void setCharge(int charge) {
+		this.charge = charge;
+	}
+		
+	public String getTravTitle() {
+		return travTitle;
+	}
+
+	public void setTravTitle(String travTitle) {
+		this.travTitle = travTitle;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public int getTotalDate() {
+		return totalDate;
+	}
+
+	public void setTotalDate(int totalDate) {
+		this.totalDate = totalDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	@Override
+	public String toString() {
+		return "AssetVo = [assetNo : "+assetNo+", travNo : "+travNo+", blogNo : "+blogNo+", visitDate : "+visitDate+", usage : "+usage+", assetCategory : "+assetCategory+", charge : "+charge+" ]";
+				}
 
 }
+
--- UsTrip/src/main/java/com/ustrip/service/plan/impl/PlanDAOImpl.java
+++ UsTrip/src/main/java/com/ustrip/service/plan/impl/PlanDAOImpl.java
@@ -1,8 +1,6 @@
 package com.ustrip.service.plan.impl;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
